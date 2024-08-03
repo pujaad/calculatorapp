d2085bf00b5a4de9e586318948b00c6b41f55d54
Puja Anand <puu5598@gmail.com>
2024-08-03 14:59:27 +0530

    made calculator app

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculator from './components/Calculator';
+
+const App=()=> {
+  return (
+    <div className='App'>
+    <h1>React Calculator</h1>
+    <div>
+    <Calculator/>
+    </div>
+    </div>
+  )
+}
+export default App;
---
+++ calculator/src/components/Calculator.js
@@ -0,0 +1,46 @@
+
+import {React,useState} from "react";
+import style from "./Calculator.css";
+const Calculator=()=>{
+    const [display,setDisplay]=useState("")
+    const appendToDisplay=(value)=>{
+        setDisplay((prev)=>prev+value);
+    }
+    const clearDisplay=()=>{
+        setDisplay("");
+    }
+    const calculate=()=>{
+        try{
+        const result=eval(display)
+        setDisplay(result.toString())
+        }catch(Error){
+            setDisplay("Error")
+        }
+    }
+    return(
+        <>
+        <div className="calculator">
+<input type="text" id={style.display}value={display} readOnly />
+<div className="buttons">
+    <button onClick={()=>appendToDisplay("7")}>7</button>
+    <button onClick={()=>appendToDisplay("8")}>8</button>
+    <button onClick={()=>appendToDisplay("9")}>9</button>
+    <button onClick={()=>appendToDisplay("+")}>+</button>
+    <button onClick={()=>appendToDisplay("4")}>4</button>
+    <button onClick={()=>appendToDisplay("5")}>5</button>
+    <button onClick={()=>appendToDisplay("6")}>6</button>
+    <button onClick={()=>appendToDisplay("-")}>-</button>
+    <button onClick={()=>appendToDisplay("1")}>1</button>
+    <button onClick={()=>appendToDisplay("2")}>2</button>
+    <button onClick={()=>appendToDisplay("3")}>3</button>
+    <button onClick={()=>appendToDisplay("*")}>*</button>
+    <button onClick={clearDisplay}>C</button>
+    <button onClick={()=>appendToDisplay("0")}>0</button>
+    <button onClick={calculate}>=</button>
+    <button onClick={()=>appendToDisplay("/")}>/</button>
+</div>
+</div>
+</>
+    )
+}
+export default Calculator;
